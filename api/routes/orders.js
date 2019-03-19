1890edfdacb1b249f8f91cfd031eece3de163385
narek.pedanyan <narek.pedanyan@betconstruct.com>
2019-03-19 13:54:03 +0400

    Parsing body/Handling cors

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -8,8 +8,13 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    };
     res.status(201).json({
-        messsage: 'Orders was created'
+        messsage: 'Orders was created',
+        order: order
     })
 });
 
